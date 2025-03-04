434dd2f666366e6c26f3bc0c5d521ac8e7d29d69
Ianshuan <1975037886@qq.com>
2025-03-04 21:43:15 +0800

    API签名认证 Client SDK引入 接口发布下线

---
+++ src/main/java/com/qian/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.qian.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qian.project.model.entity.InterfaceInfo;
+
+/**
+* @author wangyixuan
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2024-10-01 17:00:36
+* @Entity generator.domain.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/com/qian/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.qian.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qian.project.model.entity.User;
+
+/**
+ * @Entity com.yupi.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
